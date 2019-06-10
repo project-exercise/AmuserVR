3809fc92df6e5c5d27688ea9f212288ba3e94b1d
Nexus0831 <pride0831@gmail.com>
2019-06-10 11:00:16 +0900

    feat: add a docs

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,4 @@ module.exports = {
   outputDir: 'docs',
   publicPath: "./",
   assetsDir: "./",
-  // baseUrl: './',
-  // assetsDir: undefined,
-  // runtimeCompiler: undefined,
-  // productionSourceMap: undefined,
-  // parallel: undefined,
-  // css: undefined
 }
